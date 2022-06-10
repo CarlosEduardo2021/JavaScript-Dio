9b6a52b70def2329f6e36140e2f1e53e46dfeb34
CarlosEduardo2021 <carlos.ces2021@gmail.com>
2022-06-10 16:05:20 -0300

    Att01

---
+++ aula 02/script.js
@@ -0,0 +1,14 @@
+// boolean
+
+var vOuF = false;
+console.log(typeof(vOuF));
+
+// number
+var numeroq = 1;
+console.log(typeof(numeroq));
+
+//string
+var nome = 'edu';
+console.log(typeof(nome
+));
+
