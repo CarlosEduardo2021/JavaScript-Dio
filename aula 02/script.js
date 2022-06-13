78116f2fe455affd48dcafdf9713c860cb8bcd46
CarlosEduardo2021 <carlos.ces2021@gmail.com>
2022-06-13 16:16:26 -0300

    variaveis

--- aula 02/script.js
+++ aula 02/script.js
@@ -9,6 +9,16 @@ console.log(typeof(numeroq));
 
 //string
 var nome = 'edu';
-console.log(typeof(nome
-));
+console.log(typeof(nome));
 
+// como declarar 
+var variavel;
+console.log(variavel)
+
+let variavel2 ='eduardo';
+variavel2 = 'carlos';
+console.log(variavel2)
+
+// precisa ter um valor inicial, e não pode ser alterada
+const constante = 'Ana';
+console.log(constante)
