9950888b78a57b15d70a0888993194c8cb9faa63
CarlosEduardo2021 <carlos.ces2021@gmail.com>
2022-06-13 16:59:33 -0300

    variaveis

--- aula 02/script.js
+++ aula 02/script.js
@@ -22,3 +22,29 @@ console.log(variavel2)
 // precisa ter um valor inicial, e não pode ser alterada
 const constante = 'Ana';
 console.log(constante)
+
+var escopoglobal = 'global';
+console.log(escopoglobal)
+
+function escopolocal(){
+    let escopolocalinterno = 'Local';
+
+console.log(escopolocalinterno)}
+
+escopolocal();
+
+var comparacao = '0' == 0;
+console.log(comparacao);
+
+// comparação identica
+var comparacaoidentica = '0' === 0;
+console.log (comparacaoidentica);
+
+// potenciação seria 2 elevvado a 10
+var potenciacao = 2 ** 10;
+console.log(potenciacao);
+
+// && - 'e' considera que todos os valores sejam TRUE;
+// || - 'ou' considera q aualquer valor seja TRUE;
+// !-  - 'nao' Inverte o valor de TRUE para FALSE ou vice-versa
+// vetores
