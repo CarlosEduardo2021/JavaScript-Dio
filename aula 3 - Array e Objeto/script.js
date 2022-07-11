94cc79464544ab4a505c6850ba8ea9a837e8127b
Carlos Eduardo <carlos.ces2021@gmail.com>
2022-07-11 20:50:33 -0300

    array compl

--- aula 3 - Array e Objeto/script.js
+++ aula 3 - Array e Objeto/script.js
@@ -2,14 +2,51 @@
 
 // como declarar um array
 
-let array = ['string', 1, true];
-console.log(array);
+//let array = ['string', 1, true];
+//console.log(array);
 
-let array2 = ['string', 1, true, ['array'], ['array3'], ['array4']];
-console.log(array2);
+//let array2 = ['string', 1, true, ['array'], ['array3'], ['array4']];
+//console.log(array2);
 
 // selecionar um item do array, nesse caso o zero
-let array5 = ['string', 1, true, ['array'], ['array3'], ['array4']];
-console.log(array5[0]);
+let array = ['string', 1, true, ['array'], ['array3'], ['array4']];
+console.log(array[0]);
 
-// 5 min
+// forEach
+
+array.forEach(function(item, index){console.log(item, index)});
+
+// push - add um item no final do array
+
+array.push([]);
+console.log(array)
+
+// pop - apaga o ultimo item do array
+array.pop();
+console.log(array);
+
+// shift - retira um iten do inicio do array
+array.shift();
+console.log(array)
+
+// unshift - adiciona um item no inicio do array
+array.unshift('novo item');
+console.log(array);
+
+// indexOf - retorna o indice de um valor
+console.log(array.indexOf(true))
+
+// splice - remove ou substitui um item pelo indice
+array.splice(0, 3);
+console.log(array)
+
+// slice - retoma uma parte de um array existente
+let novoArray = array.slice(0, 3);
+console.log(novoArray);
+
+
+
+
+
+
+
