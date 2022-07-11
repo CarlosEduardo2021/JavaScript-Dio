80fb3992e467d51785794344d722239dd5890edd
Carlos Eduardo <carlos.ces2021@gmail.com>
2022-07-11 20:22:41 -0300

    Array

---
+++ aula 3 - Array e Objeto/script.js
@@ -0,0 +1,15 @@
+// o que são vetores ou arrays
+
+// como declarar um array
+
+let array = ['string', 1, true];
+console.log(array);
+
+let array2 = ['string', 1, true, ['array'], ['array3'], ['array4']];
+console.log(array2);
+
+// selecionar um item do array, nesse caso o zero
+let array5 = ['string', 1, true, ['array'], ['array3'], ['array4']];
+console.log(array5[0]);
+
+// 5 min
